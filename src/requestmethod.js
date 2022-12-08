9efcef26c7af8d1113ecfdea24e93621de8be439
HuyPham <105510844+HuyPham2607@users.noreply.github.com>
2022-12-08 19:11:29 +0700

    Update requestmethod.js

--- src/requestmethod.js
+++ src/requestmethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASE_URL =
-  "https://thesisloadbalancer-482548701.ap-southeast-2.elb.amazonaws.com";
+  "http://thesisloadbalancer-482548701.ap-southeast-2.elb.amazonaws.com";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
